2297c433da74084b2c7e149e8df4d21996760eae
Aidyn Zhumakadyr <aidynzhumaadyr@gmail.com>
2023-08-30 12:27:37 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TaskForm from "./TaskForm";
+import Task from "./Task";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tasks, setTasks] = useState([]);
+	useEffect(() => {
+		if (tasks.length === 0) return;
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}, [tasks]);
+	useEffect(() => {
+		const tasks = JSON.parse(localStorage.getItem("tasks"));
+		if (tasks) setTasks(tasks);
+	}, []);
+	function addTask(name) {
+		setTasks((prev) => {
+			return [...prev, { name: name, done: false }];
+		});
+	}
+	function updateComplete(index, done) {
+		const copyTasks = [...tasks];
+		copyTasks[index].done = done;
+		setTasks(copyTasks);
+	}
+	function removeTask(index) {
+		const copyTasks = (tasks) => {
+			return tasks.filter((_, taskindex) => index !== taskindex);
+		};
+		setTasks(copyTasks);
+	}
+	function renameTask(name, index) {
+		const copyTasks = [...tasks];
+		copyTasks[index].name = name;
+		setTasks(copyTasks);
+	}
+	return (
+		<main>
+			<TaskForm onAdd={addTask} />
+			{tasks.map((task, index) => (
+				<Task
+					{...task}
+					onToggle={(done) => updateComplete(index, done)}
+					onTrash={() => removeTask(index)}
+					onRename={(name) => renameTask(name, index)}
+				/>
+			))}
+		</main>
+	);
 }
 
 export default App;
